d7f44e8de86e68dea59146689fbe835921bd475f
Survidia <developer.elias.essadak@gmail.com>
2025-06-17 21:21:01 +0200

    inicio

---
+++ src/main/java/survidia/gestiongastos/GestiongastosApplication.java
@@ -0,0 +1,13 @@
+package survidia.gestiongastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiongastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiongastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/survidia/gestiongastos/GestiongastosApplicationTests.java
@@ -0,0 +1,13 @@
+package survidia.gestiongastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiongastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
